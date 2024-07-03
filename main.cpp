bbf29ffcaf608a88e75134b6bebe357df6e5e64f
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-07-02 18:45:48 -0700

    base

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 
-class meal()
+class Meal
+{
+	int calorie, protein, day;
+public:
+	meal(int &calorie, int &protein, int &day)
+{
+	std::cout << "WORKS" << std::endl;
 {
 
 }
 
 int main(int argc, char* argv[])
 {
-	int calorie;
-	int protein;
-	int meal = calorie + protein;
+	int calorie, protein, day;
+	day = 69;
 	std::cout << "Enter calorie for food, then hit enter" << std::endl;
 	std::cin >> calorie;
 	std::cout << "Now, enter the grams of protein of entered food." << std::endl;
 	std::cin >> protein;
-	std::cout << "First entry calorie:" << calorie << " protein " << protein << std::endl;
-	std::cout << meal << std::endl; 
-
+	std::cout << "Meal - " << day << "\ncalories: " << calorie << "\nprotein: " << protein << std::endl;
 }
