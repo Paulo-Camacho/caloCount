274901cf6fb41090fc7f5fde92fc42dff75aac51
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-07-07 18:09:28 -0700

    new new base

--- main.cpp
+++ main.cpp
@@ -1,49 +1,66 @@
 #include <iostream>
 
 struct Meal {
+    int calorie;
+    int protein;
+    int day = 1;
 
-	int calorie, protein, day;
-
-	void meal() {
-	std::cout << "Enter calorie for food, then hit enter" << std::endl;
-	std::cin >> calorie;
-	std::cout << "Now, enter the grams of protein of entered food." << std::endl;
-	std::cin >> protein;
-	std::cout << "Meal - " << day << "\ncalories: " << calorie << "\nprotein: " << protein << std::endl;
-	}
+    void meal() {
+        std::cout << " " << std::endl;
+        std::cout << "Enter calorie for food, then hit enter: " << std::endl;
+        std::cin  >> calorie;
+        std::cout << "Now, enter the grams of protein of entered food: " << std::endl;
+        std::cin  >> protein;
+        std::cout << " " << std::endl;
+        std::cout << "Meal - " << day << "\ncalories: " << calorie << "\nprotein: " << protein << std::endl;
+        day++;
+    }
 };
 
-class Loop{
+class Menu {
 public:
-	int userStart;
-	bool program = true;
-	Meal entry;
-
-	void suma() {
-		while(program) {
-		std::cout << "1 to start/continue or 0 to quit " << std::endl;
-		std::cin >> userStart;
-				if(userStart = 1) {
-				entry.meal();
-					} else if (userStart = 0) {
-					std::cout << "Have a good day! " << std::endl;
-					program = false;
-						} else {
-						std::cout << "Make sure you type either 1 or 0 " << std::endl;
-			}
-		}
-	}
+    Meal entry;
+    int question = 1;
+    int iteration;
+    void display(bool& program) {  // Pass by reference
+        if(question == 1) {
+            std::cout << "Enter 1 to start, 0 to quit " << std::endl;
+            std::cin >> iteration;
+            question = 0;
+            }        
+            if(iteration == 1) {
+                entry.meal();  
+            } else if (iteration == 0) {
+                std::cout << "Good bye, have a good day! " << std::endl;
+                program = false;
+            }
+    }
 };
 
-class Prompt{
+class Loop {
 public:
-	Loop menu;
-	void display() {
-		menu.suma();	
-	}
+    Menu start;
+    bool program = true;
+    bool cont;
+
+    void suma() {
+        while(program) {
+            start.display(program);
+            std::cout << " " << std::endl;
+            std::cout << "Continue? 1/0" << std::endl;
+            std::cin >> cont;
+            if(cont == 1) {
+                start.display(program);
+            } else {
+                std::cout << "Have a good day! " << std::endl;
+                program = false;
+            } 
+        }
+    }
 };
 
 int main(int argc, char* argv[]) {
-	Prompt test;
-	test.display();
+    Loop test;
+    test.suma();
 }
+
