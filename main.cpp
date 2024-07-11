bfb349e8e11abb5320833e6d7a3ecd97c0f4674d
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-07-10 20:24:20 -0700

    iteration being started

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 struct Meal {
-    int calorie;
-    int protein;
+    int calorie = 0;
+    int protein = 0;
+    int totalProtein = 0;
+    int totalCalorie = 0;
     int day = 1;
 
-    void meal() {
+    void mealInput() {
         std::cout << " " << std::endl;
         std::cout << "Enter calorie for food, then hit enter: " << std::endl;
         std::cin  >> calorie;
@@ -14,6 +16,9 @@ struct Meal {
         std::cout << " " << std::endl;
         std::cout << "Meal - " << day << "\ncalories: " << calorie << "\nprotein: " << protein << std::endl;
         day++;
+
+    std::vector<Diary> diary;
+        
     }
 };
 
@@ -21,26 +26,28 @@ class Menu {
 public:
     Meal entry;
     int prompt = 0;
-    int pass = 0;
+    int pass   = 0;
     int resume = 0;
     void display(bool& program) {  // Pass by reference
         if(prompt == 0) {
-            std::cout << "Hello! Would you like to count? 1/yes 0/no" << std::endl; 
-            std::cin >> prompt;
-            pass ++;
+            std::cout << "Hello! Would you like to count? 1/yes 0/no " << std::endl; 
+            std::cin >> pass;
+            prompt = 1;
         }
         if(pass == 0) {
-            std::cout << "Have a good day! " << std::endl;
+            std::cout << " " << std::endl;
+            std::cout << "Have a great day! " << std::endl;
             program = false;
         } else if(pass == 1) {
-            entry.meal();
+            entry.mealInput();
         } else {
             std::cout << "Make sure to enter EITHER 1 or 0 " << std::endl;
             program = false;
         }
 
         if(pass == 1) { 
-            std::cout << "Would you like to continue? " << std::endl;
+            std::cout << " " << std::endl;
+            std::cout << "Continue to add? 1/yes 0/no " << std::endl;
             std::cin >> pass; 
         }
         }
