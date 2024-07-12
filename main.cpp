b7c974f82dc649b79f74adeba6127cd245fec15d
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-07-11 18:55:20 -0700

    fixed error handling

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 
 struct Meal {
@@ -11,17 +12,27 @@ struct Meal {
     void mealInput() {
         std::cout << " " << std::endl;
         std::cout << "Enter calorie for food, then hit enter: " << std::endl;
-        std::cin  >> calorie;
+        while (!(std::cin >> calorie) || calorie < 0) {
+            std::cout << "Invalid input. Please enter a positive integer for calories: " << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
         std::cout << "Now, enter the grams of protein of entered food: " << std::endl;
-        std::cin  >> protein;
+        while (!(std::cin >> protein) || protein < 0) {
+            std::cout << "Invalid input. Please enter a positive integer for protein: " << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
         std::cout << " " << std::endl;
         std::cout << "Meal - " << day << "\ncalories: " << calorie << "\nprotein: " << protein << std::endl;
+
         day++;
         totalCalories += calorie;
         totalProtein += protein;
         std::cout << " " << std::endl;
-        std::cout << "Total protein " << totalProtein << "\nTotal calories " << totalCalories << std::endl;
-
+        std::cout << "Total protein: " << totalProtein << "\nTotal calories: " << totalCalories << std::endl;
     }
 };
 
@@ -30,13 +41,15 @@ public:
     Meal entry;
     int prompt = 0;
     int pass   = 0;
-    int resume = 0;
-
 
     void display(bool& program) {  // Pass by reference
         if(prompt == 0) {
-            std::cout << "Hello! Would you like to count? 1/yes 0/no " << std::endl; 
-            std::cin >> pass;
+            std::cout << "Hello! Would you like to count? 1/yes 0/no " << std::endl;
+            while (!(std::cin >> pass) || (pass != 0 && pass != 1)) {
+                std::cout << "Invalid input. Please enter 1 for yes or 0 for no: " << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
             prompt = 1;
         }
         if(pass == 0) {
@@ -45,17 +58,22 @@ public:
             program = false;
         } else if(pass == 1) {
             entry.mealInput();
+            std::cout << " " << std::endl;
+            std::cout << "Continue to add? 1/yes 0/no " << std::endl;
+            while (!(std::cin >> pass) || (pass != 0 && pass != 1)) {
+                std::cout << "Invalid input. Please enter 1 for yes or 0 for no: " << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            if (pass == 0) {
+                program = false;
+                std::cout << "Have a great day! " << std::endl;
+            }
         } else {
             std::cout << "Make sure to enter EITHER 1 or 0 " << std::endl;
             program = false;
         }
-
-        if(pass == 1) { 
-            std::cout << " " << std::endl;
-            std::cout << "Continue to add? 1/yes 0/no " << std::endl;
-            std::cin >> pass; 
-        }
-        }
+    }
 };
 
 class Loop {
@@ -70,8 +88,7 @@ public:
     }
 };
 
-int main(int argc, char* argv[]) {
+int main() {
     Loop test;
     test.suma();
 }
-
